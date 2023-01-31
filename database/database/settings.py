39692ecae0fa9efb7eda0e2c59521808e0ed3bb2
AbisaiBH <90496084+AbisaiBH@users.noreply.github.com>
2023-01-30 23:48:49 -0600

    Save

--- database/database/settings.py
+++ database/database/settings.py
@@ -18,10 +18,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-aj3z8g$&$_5ttdi4!2zfa=ak^@!n8mgb0mns^1s8yef^t_ym5a'
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
